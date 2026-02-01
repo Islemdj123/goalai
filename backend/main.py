1e06cc45e216571d8c36e036c4d3b40b3e9e8f98
Islemdj123 <islemdjeridi5@gmail.com>
2026-02-01 19:21:00 +0300

    "FixCORS"

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app.mount("/data", StaticFiles(directory="data"), name="data")
 # Enable CORS for Next.js
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
